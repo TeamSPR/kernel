a494f6f92c428a5f4b8e390031f1814766a07260
ktoonsez <ktoonsez@gmail.com>
2015-05-12 21:16:55 -0700

    cpufreq: Add ktoonservative governor Add GPU hook to boost GPU Add hook to ktoonservative from main input booster so it will boost from all hardware devices Make dm hotplug aware of ktoonservative status Add global var to cpu_up and cpu_down functions to help identify the real cpu core that just went up or down on link policies

--- drivers/cpufreq/dm_cpu_hotplug.c
+++ drivers/cpufreq/dm_cpu_hotplug.c
@@ -554,7 +554,6 @@ static int fb_state_change(struct notifier_block *nb,
 	case FB_BLANK_POWERDOWN:
 		lcd_is_on = false;
 		pr_info("LCD is off\n");
-		screen_is_on = false;
 
 #ifdef CONFIG_HOTPLUG_THREAD_STOP
 		if (thread_manage_wq) {
@@ -572,7 +571,6 @@ static int fb_state_change(struct notifier_block *nb,
 		 * turned on.
 		 */
 		lcd_is_on = true;
-		screen_is_on = true;
 		pr_info("LCD is on\n");
 
 #ifdef CONFIG_HOTPLUG_THREAD_STOP
@@ -602,7 +600,9 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 #if defined(CONFIG_SCHED_HMP)
 	int hotplug_out_limit = 0;
 #endif
-
+	if (ktoonservative_is_active && ktoonservative_hp_active)
+		return 0;
+	
 	if (exynos_dm_hotplug_disabled())
 		return 0;
 
