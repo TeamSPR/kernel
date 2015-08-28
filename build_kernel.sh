5c610ee1f20eb87b653ab4a342d4228ef7eeb878
UpInTheAir <upintheair.xda@gmail.com>
2015-08-28 16:27:07 +0700

    Initial setup for compile

---
+++ build_kernel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ARCH=arm64
+
+# Clean up
+make clean
+make mrproper
+ccache -c
+
+# Set toolchain
+export CROSS_COMPILE=/home/upintheair/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+# Make .config
+make ARCH=arm64 SkyHigh_defconfig
+
+# Compile Image
+make -j5 ARCH=arm64
