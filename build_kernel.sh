72d0e40b232c225cf6e020514d73067311079ebe
UpInTheAir <upintheair.xda@gmail.com>
2015-08-29 21:08:03 +0700

    build_kernel.sh: make SkyHigh_tmo_defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -12,6 +12,8 @@ export CROSS_COMPILE=/home/upintheair/aarch64-linux-android-4.9/bin/aarch64-linu
 
 # Make .config
 make ARCH=arm64 SkyHigh_defconfig
+# T-Mobile
+#make ARCH=arm64 SkyHigh_tmo_defconfig
 
 # Compile Image
 make -j5 ARCH=arm64
