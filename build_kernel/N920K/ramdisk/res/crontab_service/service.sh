d27cd0389e6904bb84a1615e76ea6db71243b63d
UpInTheAir <upintheair.xda@gmail.com>
2016-01-17 21:12:45 +0700

    ramdisk: cron: TZ coding & style fixes
    thanks to @dorimanx https://github.com/dorimanx/lp_ramdisk_dorimanx/commit/691f43a7bd6315479fb142082da1631ea605edc8
    
    Signed-off-by: UpInTheAir <upintheair.xda@gmail.com>

--- build_kernel/N920K/ramdisk/res/crontab_service/service.sh
+++ build_kernel/N920K/ramdisk/res/crontab_service/service.sh
@@ -26,88 +26,88 @@ chown 0:0 /var/spool/cron/crontabs/*;
 chmod 777 /var/spool/cron/crontabs/*;
 
 # Check device local timezone & set for cron tasks
-timezone=`date +%z`
-if $BB [ $timezone = "+1400" ]; then
+timezone=$(date +%z);
+if [ "$timezone" == "+1400" ]; then
 	TZ=UCT-14
-elif $BB [ $timezone = "+1300" ]; then
+elif [ "$timezone" == "+1300" ]; then
 	TZ=UCT-13
-elif $BB [ $timezone = "+1245" ]; then
+elif [ "$timezone" == "+1245" ]; then
 	TZ=CIST-12:45CIDT
-elif $BB [ $timezone = "+1200" ]; then
+elif [ "$timezone" == "+1200" ]; then
 	TZ=NZST-12NZDT
-elif $BB [ $timezone = "+1100" ]; then
+elif [ "$timezone" == "+1100" ]; then
 	TZ=UCT-11
-elif $BB [ $timezone = "+1030" ]; then
-	TZ= LHT-10:30LHDT
-elif $BB [ $timezone = "+1000" ]; then
+elif [ "$timezone" == "+1030" ]; then
+	TZ=LHT-10:30LHDT
+elif [ "$timezone" == "+1000" ]; then
 	TZ=UCT-10
-elif $BB [ $timezone = "+0930" ]; then
+elif [ "$timezone" == "+0930" ]; then
 	TZ=UCT-9:30
-elif $BB [ $timezone = "+0900" ]; then
+elif [ "$timezone" == "+0900" ]; then
 	TZ=UCT-9
-elif $BB [ $timezone = "+0830" ]; then
+elif [ "$timezone" == "+0830" ]; then
 	TZ=KST
-elif $BB [ $timezone = "+0800" ]; then
+elif [ "$timezone" == "+0800" ]; then
 	TZ=UCT-8
-elif $BB [ $timezone = "+0700" ]; then
+elif [ "$timezone" == "+0700" ]; then
 	TZ=UCT-7
-elif $BB [ $timezone = "+0630" ]; then
+elif [ "$timezone" == "+0630" ]; then
 	TZ=UCT-6:30
-elif $BB [ $timezone = "+0600" ]; then
+elif [ "$timezone" == "+0600" ]; then
 	TZ=UCT-6
-elif $BB [ $timezone = "+0545" ]; then
+elif [ "$timezone" == "+0545" ]; then
 	TZ=UCT-5:45
-elif $BB [ $timezone = "+0530" ]; then
+elif [ "$timezone" == "+0530" ]; then
 	TZ=UCT-5:30
-elif $BB [ $timezone = "+0500" ]; then
+elif [ "$timezone" == "+0500" ]; then
 	TZ=UCT-5
-elif $BB [ $timezone = "+0430" ]; then
+elif [ "$timezone" == "+0430" ]; then
 	TZ=UCT-4:30
-elif $BB [ $timezone = "+0400" ]; then
+elif [ "$timezone" == "+0400" ]; then
 	TZ=UCT-4
-elif $BB [ $timezone = "+0330" ]; then
+elif [ "$timezone" == "+0330" ]; then
 	TZ=UCT-3:30
-elif $BB [ $timezone = "+0300" ]; then
+elif [ "$timezone" == "+0300" ]; then
 	TZ=UCT-3
-elif $BB [ $timezone = "+0200" ]; then
+elif [ "$timezone" == "+0200" ]; then
 	TZ=UCT-2
-elif $BB [ $timezone = "+0100" ]; then
+elif [ "$timezone" == "+0100" ]; then
 	TZ=UCT-1
-elif $BB [ $timezone = "+0000" ]; then
+elif [ "$timezone" == "+0000" ]; then
 	TZ=UCT
-elif $BB [ $timezone = "-0100" ]; then
+elif [ "$timezone" == "-0100" ]; then
 	TZ=UCT1
-elif $BB [ $timezone = "-0200" ]; then
+elif [ "$timezone" == "-0200" ]; then
 	TZ=UCT2
-elif $BB [ $timezone = "-0300" ]; then
+elif [ "$timezone" == "-0300" ]; then
 	TZ=UCT3
-elif $BB [ $timezone = "-0330" ]; then
+elif [ "$timezone" == "-0330" ]; then
 	TZ=NST3:30NDT
-elif $BB [ $timezone = "-0400" ]; then
+elif [ "$timezone" == "-0400" ]; then
 	TZ=UCT4
-elif $BB [ $timezone = "-0430" ]; then
+elif [ "$timezone" == "-0430" ]; then
 	TZ=UCT4:30
-elif $BB [ $timezone = "-0500" ]; then
+elif [ "$timezone" == "-0500" ]; then
 	TZ=UCT5
-elif $BB [ $timezone = "-0600" ]; then
+elif [ "$timezone" == "-0600" ]; then
 	TZ=UCT6
-elif $BB [ $timezone = "-0700" ]; then
+elif [ "$timezone" == "-0700" ]; then
 	TZ=UCT7
-elif $BB [ $timezone = "-0800" ]; then
+elif [ "$timezone" == "-0800" ]; then
 	TZ=UCT8
-elif $BB [ $timezone = "-0900" ]; then
+elif [ "$timezone" == "-0900" ]; then
 	TZ=UCT9
-elif $BB [ $timezone = "-0930" ]; then
+elif [ "$timezone" == "-0930" ]; then
 	TZ=UCT9:30
-elif $BB [ $timezone = "-1000" ]; then
+elif [ "$timezone" == "-1000" ]; then
 	TZ=UCT10
-elif $BB [ $timezone = "-1100" ]; then
+elif [ "$timezone" == "-1100" ]; then
 	TZ=UCT11
-elif $BB [ $timezone = "-1200" ]; then
+elif [ "$timezone" == "-1200" ]; then
 	TZ=UCT12
 else
 	TZ=UCT
-fi
+fi;
 
 export TZ
 
